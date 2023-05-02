21773232f3b4407dec007fc330aec832b0af63e2
Annie Vargas <annie.vargas94@gmail.com>
2023-05-02 17:48:02 -0600

    Initial commit

--- 1.js
+++ 1.js
@@ -2,12 +2,20 @@
 // Dada una matriz de N elementos en la que todos los elementos son iguales excepto uno,
 // crea una función findUniq que retorne el elemento único.
 
-function findUniq(array) {
-}
-
+function findUniq(array) {}
 
 /**
  * TEST Ejercicio 1
  */
-findUniq(['12', 10, '12', 11, 1, 11, 10, '12']) // 1
-findUniq(['Capitán América', 'Hulk', 'Deadpool', 'Capitán América', 'Hulk', 'Wonder Woman', 'Deadpool', 'Iron Man', 'Iron Man']) // 'Wonder Woman'
+findUniq(["12", 10, "12", 11, 1, 11, 10, "12"]); // 1
+findUniq([
+  "Capitán América",
+  "Hulk",
+  "Deadpool",
+  "Capitán América",
+  "Hulk",
+  "Wonder Woman",
+  "Deadpool",
+  "Iron Man",
+  "Iron Man",
+]); // 'Wonder Woman'
